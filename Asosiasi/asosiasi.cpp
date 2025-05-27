3be25f48224e625595d8140ee66bd65d20672cf8
Cruelasss <irula5198@gmail.com>
2025-05-27 11:09:36 +0700

    implementasi relasi dua arah antara pasien dan dokter

--- Asosiasi/asosiasi.cpp
+++ Asosiasi/asosiasi.cpp
@@ -22,7 +22,7 @@ class pasien{
 class dokter{
     public:
     string nama;
-     vector<dokter*> daftar_dokter;
+     vector<pasien*> daftar_pasien;
     dokter(string pNama) :nama(pNama)
     {
         cout << "Dokter \"" << nama << "\" ada\n";
@@ -34,3 +34,32 @@ class dokter{
         void tambahPasien(pasien*);
         void cetakPasien();
 };
+
+void pasien::tambahDokter(dokter *pDokter)
+{
+    daftar_dokter.push_back(pDokter);
+}
+void pasien::cetakDokter()
+{
+    cout << "Daftar Dokter yang menangani pasien \"" << this->nama << "\":\n";
+    for (auto &a : daftar_dokter)
+    {
+        cout << a->nama << "\n";
+    }
+    cout << endl;
+}
+void dokter::tambahPasien(pasien *pPasien)
+{
+    daftar_pasien.push_back(pPasien);
+    pPasien->tambahDokter(this);
+}
+void dokter::cetakPasien()
+{
+    cout << "Daftar pasien dari dokter \"" << this->nama << "\":\n";
+    for (auto &a : daftar_pasien)
+    {
+        cout << a->nama << nama << "\n";
+    }
+    cout << endl;
+}
+
