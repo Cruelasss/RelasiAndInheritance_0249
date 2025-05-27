8c013d2f5091cf2f8ee3e75c81b25dde581f3f19
Cruelasss <irula5198@gmail.com>
2025-05-27 10:30:23 +0700

    perubahan

--- Agregasi/ibu.h
+++ Agregasi/ibu.h
@@ -24,15 +24,11 @@ void ibu::tambahAnak(anak *pAnak)
 }
 
 void ibu::cetakAnak() {
-    cout << "daftar Anak dari Ibu \"" << 
-    this->nama << "\":\n";
-
+    cout << "daftar Anak dari Ibu \"" << this->nama << "\":\n";
     for (int i=0; i<daftar_anak.size(); i++) {
         cout << daftar_anak[i]->nama << endl;
 } 
     cout << endl;
-
 }
-
 #endif
 
